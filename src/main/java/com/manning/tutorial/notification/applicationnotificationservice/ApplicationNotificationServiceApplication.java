7030d7eb65b4fd392bed4a1d53d20d34d2544752
Kacper Pietrasik <pietrasikk@gmail.com>
2022-01-02 15:53:35 +0100

    Init project

---
+++ src/main/java/com/manning/tutorial/notification/applicationnotificationservice/ApplicationNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.applicationnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manning/tutorial/notification/applicationnotificationservice/ApplicationNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.applicationnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
